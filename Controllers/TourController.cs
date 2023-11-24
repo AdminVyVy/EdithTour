6884affd5dde9c4afe0abbf580c556892a1f14e8
AdminVyVy <vy0799608976@gmail.com>
2023-11-24 19:31:41 +0700

    1.2.0.1

---
+++ Controllers/TourController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EdithTour.Controllers
+{
+    public class TourController : Controller
+    {
+        // GET: Tour
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Test()
+        {
+            return View();
+        }
+
+        public ActionResult Danang()
+        {
+            return View();
+        }
+        public ActionResult Quangbinh()
+        {
+            return View();
+        }
+
+        public ActionResult Hanoi()
+        {
+            return View();
+        }
+        public ActionResult Nhatrang()
+        {
+            return View();
+        }
+        public ActionResult Saigon()
+        {
+            return View();
+        }
+        
+    }
+}
